5a0e65b9455f9b00bb33e27a984b204228b69752
Apparao Kalimireddy <apparaokalimireddy@gmail.com>
2017-04-02 17:17:01 -0500

    Changes

--- main.R
+++ main.R
@@ -8,6 +8,7 @@ library(scales)
 
 simple <- qplot(x = df$Day, y = df$Revenues, geom="point", ylab="Revenues") 
 simple <- simple + scale_y_continuous(name = "Revenues", labels = dollar) 
-simple <- simple + scale_x_continuous(name = "Day")
+simple <- simple + scale_x_continuous(name = "Day", 
+                                      breaks = round(seq(min(df$Day), max(df$Day), by = 1),1))
 
 
