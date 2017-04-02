35a1a1c86e1924ce4dacc97253cc5e8d543b7843
Apparao Kalimireddy <apparaokalimireddy@gmail.com>
2017-04-02 18:04:26 -0500

    Changes

--- main.R
+++ main.R
@@ -6,9 +6,10 @@ df<-loadAndPrepare("wine21.csv")
 library(ggplot2)
 library(scales)
 
+# Todo: It would be nice to have color regions to indicate holidays and weekends
 simple <- qplot(x = df$Day, y = df$Revenues, geom="point", ylab="Revenues") 
 simple <- simple + scale_y_continuous(name = "Revenues", labels = dollar) 
 simple <- simple + scale_x_continuous(name = "Day", 
                                       breaks = round(seq(min(df$Day), max(df$Day), by = 1),1))
-
+simple <- simple + geom_smooth(method = "lm", se=FALSE, color="blue", aes(group=1))
 
